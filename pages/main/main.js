7336514366e32a4f83089fff5497af9279c98e4e
llb <tongxinlubo@163.com>
2016-12-04 23:43:51 +0800

    first commit

---
+++ pages/main/main.js
@@ -0,0 +1,24 @@
+Page({
+  data:{
+    swiperImgUrls:[
+        'http://ent.hangzhou.com.cn/images/attachement/jpg/site2/20161123/bc305bb44f14199f337611.jpg',
+        'http://img1.imgtn.bdimg.com/it/u=3330999059,2221651524&fm=11&gp=0.jpg',
+        'http://f.hiphotos.baidu.com/image/pic/item/b151f8198618367a9f738e022a738bd4b21ce573.jpg'
+    ]
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
